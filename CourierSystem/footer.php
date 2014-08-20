828e152593afcf2e9a1dae08a6ab27fbaad2f70c
prajakta-webonise <prajakta.rajapure@weboniselab.com>
2014-08-20 15:57:56 +0530

    Delete

--- CourierSystem/footer.php
+++
@@ -1,7 +0,0 @@
-		</div>
-        <script src="js/bootstrap.min.js"></script>
-
-        <p class="text-center">&copy 2014 Webonise Labs <a href="#">Privacy Policy</a></p>
-    </body>
-    
-</html>
